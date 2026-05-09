7f319fcaf5f388ab6fd9b6a4d36c9e7c80e2f358
Maheshwara Reddy <maheshwara1319@gmail.com>
2026-05-10 02:44:35 +0530

    Add Playwright job extraction prototype

--- playwrightTest.js
+++ playwrightTest.js
@@ -4,9 +4,28 @@ async function openBrowser() {
   const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto("https://example.com");
+  await page.goto("https://careers.microsoft.com/us/en/search-results");
 
-  console.log("Page opened successfully");
+  const pageTitle = await page.title();
+
+
+const jobCardText = await page
+  .getByRole('button', { name: /View job:/ })
+  .first()
+  .innerText();
+
+const lines = jobCardText.split("\n");
+
+const jobData = {
+  title: lines[0],
+  location: lines[1],
+  posted: lines[2]
+};
+
+console.log(jobData);
+
+// console.log(lines);
+
 
   await browser.close();
 }
